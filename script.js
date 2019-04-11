dbd659374d9a0534a5a9baf47a46556dc1692e0a
Andreea Zamfir <zamfir.andreea.maria@gmail.com>
2019-04-11 14:52:51 +0300

    Updated files with final code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.getElementsByClassName("container")[0];
 const titleInput = document.getElementsByClassName("title")[0];
 const descriptionInput = document.getElementsByClassName("description")[0];
+const colorInput = document.getElementsByClassName("color")[0];
 const cancelBtn = document.getElementsByClassName("cancel_btn")[0];
 const submitBtn = document.getElementsByClassName("submit_btn")[0];
 const buttonSection = document.getElementsByClassName("button_section")[0];
@@ -36,8 +37,8 @@ const clearNoteForm = () => {
         .style.display = "none";
     buttonSection
         .style.display = "none";
-    titleInput.value = '';
-    descriptionInput.value = '';
+    titleInput.innerText = '';
+    descriptionInput.innerText = '';
     document.getElementsByClassName("note")[0].className = "note";
 };
 
@@ -48,9 +49,14 @@ const addNoteAddListener = () => {
 
 const addNoteEvent = () => {
     const newNote = getNewNoteValues();
-    createNote(newNote);
-    clearNoteForm();
-    saveNoteData(newNote);
+    if(getNewNoteValues().description.length !== 0 || getNewNoteValues().title.length !== 0){
+        createNote(newNote);
+        clearNoteForm();
+        saveNoteData(newNote);
+    } else {
+    alert('The note is empty!');
+}
+
 };
 
 const saveNoteData = (noteData) => {
@@ -61,8 +67,8 @@ const saveNoteData = (noteData) => {
 
 const createNote = (newNote) => {
     const noteSectionCmp = document.createElement("section");
-    const noteTitleCmp = document.createElement("h2");
-    const noteDescriptionCmp = document.createElement("p");
+    const noteTitleCmp = document.createElement("div");
+    const noteDescriptionCmp = document.createElement("div");
     const actionsSectionCmp = document.createElement("section");
     const deleteBtn = document.createElement("button");
 
@@ -72,10 +78,21 @@ const createNote = (newNote) => {
     actionsSectionCmp.className = "actions_section";
     deleteBtn.className = "delete_btn";
 
+    noteTitleCmp.contentEditable = "true";
+    noteDescriptionCmp.contentEditable = "true";
+
     deleteBtn.type = "reset";
     deleteBtn.innerHTML = "Delete";
 
+    noteTitleCmp.addEventListener("input", updateNoteTitle);
+    noteDescriptionCmp.addEventListener("input", updateNoteDescription);
+
     addNoteValues(noteTitleCmp, noteDescriptionCmp, newNote);
+
+    const colorClass = determineNoteColorCLass(newNote.color);
+    if(colorClass) {
+        noteSectionCmp.classList.add(colorClass);
+    }
 
     actionsSectionCmp.appendChild(deleteBtn);
     noteSectionCmp.appendChild(noteTitleCmp);
@@ -87,15 +104,34 @@ const createNote = (newNote) => {
     deleteBtn.addEventListener("click", deleteEvent);
 };
 
+
+const updateNoteTitle = (event) => {
+    let inputField = event.currentTarget;
+    const noteData = JSON.parse(inputField.parentNode.getAttribute("data"));
+    deleteNoteData(noteData);
+    noteData.title = inputField.innerText;
+    inputField.parentNode.setAttribute("data", JSON.stringify(noteData));
+    saveNoteData(noteData);
+};
+const updateNoteDescription = (event) => {
+    let inputField = event.currentTarget;
+    const noteData = JSON.parse(inputField.parentNode.getAttribute("data"));
+    deleteNoteData(noteData);
+    noteData.description = inputField.innerText;
+    inputField.parentNode.setAttribute("data", JSON.stringify(noteData));
+    saveNoteData(noteData);
+};
+
 const addNoteValues = (title, description, note) => {
-    title.innerHTML = note.title;
-    description.innerHTML = note.description;
+        title.innerText = note.title;
+        description.innerText = note.description;
 };
 
 const getNewNoteValues = () => {
     return {
-        title: titleInput.value,
-        description: descriptionInput.value
+        title: titleInput.innerText,
+        description: descriptionInput.innerText,
+        color: colorInput.value
     };
 };
 
@@ -129,39 +165,43 @@ const initColorListeners = () => {
     colorsList.forEach(color => color.addEventListener("click", changeColor));
 };
 
-const changeColor = (event) => {
-    const formColor = event.currentTarget.getAttribute("color");
-    const noteComp = document.getElementsByClassName("note")[0]
+let noteDefaultClasses='';
+
+const determineNoteColorCLass = (formColor) => {
     switch (formColor) {
         case "white":
-            noteComp.className = "note";
-            noteComp.classList.add("white_background");
-            break;
+            return "white_background";
         case "red":
-            noteComp.className = "note";
-            noteComp.classList.add("red_background");
-            break;
+            return "red_background";
         case "blue":
-            noteComp.className = "note";
-            noteComp.classList.add("blue_background");
-            break;
+            return "blue_background";
         case "green":
-            noteComp.className = "note";
-           noteComp.classList.add("green_background");
-            break;
+            return "green_background";
         case "purple":
-            noteComp.className = "note";
-            noteComp.classList.add("purple_background");
-            break;
+            return "purple_background";
         case "yellow":
-            noteComp.className = "note";
-           noteComp.classList.add("yellow_background");
-            break;
+            return "yellow_background";
+    }
+     return null;
+};
+
+const changeColor = (event) => {
+    const noteComp = document.getElementsByClassName("note")[0];
+    if(noteDefaultClasses.length === 0){
+     noteDefaultClasses = noteComp.className;
+    }
+    const formColor = event.currentTarget.getAttribute("color");
+
+    noteComp.className = noteDefaultClasses;
+
+    let newClassToAdd = determineNoteColorCLass(formColor);
+    colorInput.value = formColor;
+    if(newClassToAdd){
+        noteComp.classList.add(newClassToAdd);
     }
 };
 
 const init = () => {
-
     initNoteForm();
     loadSavedNotes();
     initColorListeners();
